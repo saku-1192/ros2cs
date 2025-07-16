d021e433bc2cbd8ab6f9afaaaba035cea8f909e3
saku-1192 <sakusaku061110@gmail.com>
2025-07-16 15:17:05 +0900

    Fix .repos

--- src/ros2cs/rosidl_generator_cs/cmake/rosidl_generator_cs_generate_interfaces.cmake
+++ src/ros2cs/rosidl_generator_cs/cmake/rosidl_generator_cs_generate_interfaces.cmake
@@ -245,7 +245,7 @@ foreach(_generated_msg_c_ts_file ${_generated_msg_c_ts_files})
 
   set(ros2_distro "$ENV{ROS_DISTRO}")
 
-  if(ros2_distro STREQUAL "jazzy" OR ros2_distro STREQUAL "rolling")
+  if(${rosidl_cmake_VERSION} VERSION_GREATER 2.5.0)
     rosidl_get_typesupport_target(c_typesupport_target "${PROJECT_NAME}" "rosidl_typesupport_c")
     target_link_libraries(${_target_name} "${c_typesupport_target}")
   else()
@@ -352,7 +352,7 @@ foreach(_generated_srv_c_ts_file ${_generated_srv_c_ts_files})
 
   set(ros2_distro "$ENV{ROS_DISTRO}")
 
-  if(ros2_distro STREQUAL "jazzy" OR ros2_distro STREQUAL "rolling")
+  if(${rosidl_cmake_VERSION} VERSION_GREATER 2.5.0)
     rosidl_get_typesupport_target(c_typesupport_target "${PROJECT_NAME}" "rosidl_typesupport_c")
     target_link_libraries(${_target_name} "${c_typesupport_target}")
   else()
